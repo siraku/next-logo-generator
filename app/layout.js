ebb6f76354ad1d180d5f6346fbf1060438f450e0
siraku <slstc6666@yahoo.co.jp>
2025-02-11 22:14:51 +0900

    change font

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Vollkorn } from "next/font/google";
 import "./globals.css";
 import Provider from "./provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const vollkorn = Vollkorn({
   subsets: ["latin"],
 });
 
@@ -20,9 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={vollkorn.className}>
         <Provider>{children}</Provider>
       </body>
     </html>
